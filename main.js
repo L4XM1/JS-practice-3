ec31eeb71f02f72cfd42e358382294d9a15df140
Marta Maksimovic <marta.maksimovic1988@gmail.com>
2023-03-19 15:44:26 +0100

    feat: promises

--- main.js
+++ main.js
@@ -138,3 +138,62 @@ let add = (a, b) => {
 };
 
 add(12, 2);
+
+//Promises (obj used for deferred and async /operation that hasn't completed yet but is expected in the future)
+
+//Immediatley Resolved
+var myPromise = Promise.resolve("Foo");
+
+myPromise.then((res) => console.log(res));
+
+var mySecondPromise = new Promise(function (resolve, reject) {
+  setTimeout(() => resolve(4), 2000);
+});
+
+mySecondPromise.then((res) => {
+  res += 3;
+  console.log(res);
+});
+
+//fetch data from an API
+function getData(method, url) {
+  return new Promise(function (resolve, reject) {
+    var xhr = new XMLHttpRequest();
+    xhr.open(method, url);
+    xhr.onload = function () {
+      if (this.status >= 200 && this.status < 300) {
+        resolve(xhr.response);
+      } else {
+        reject({
+          status: this.status,
+          statusText: xhr.statusText,
+        });
+      }
+    };
+    xhr.onerror = function () {
+      reject({
+        status: this.status,
+        statusText: xhr.statusText,
+      });
+    };
+    xhr.send();
+  });
+}
+
+getData("GET", "https://jsonplaceholder.typicode.com/todos")
+  .then(function (data) {
+    let todos = JSON.parse(data);
+    let output = "";
+    for (let todo of todos) {
+      output += `
+      <li>
+        <h3>${todo.title}</h3>
+        <p>${todo.completed}</p>
+      </li>
+      `;
+    }
+    document.getElementById("template").innerHTML = output;
+  })
+  .catch(function (err) {
+    console.log(err);
+  });
