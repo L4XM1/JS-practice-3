fdbf08f99f3eab9e64b84e7895ffc7a8e63e959d
Marta Maksimovic <marta.maksimovic1988@gmail.com>
2023-03-19 14:06:29 +0100

    feat: weakset and weakmap

--- main.js
+++ main.js
@@ -20,7 +20,7 @@ test(...args1, ...args2);
 
 //Set, Map, WeakSet and WeakMap
 
-//set (values)
+//Set (values)
 let myArray = [11, 22, 34, 65, 34];
 let mySet = new Set(myArray);
 
@@ -36,7 +36,7 @@ mySet.forEach(function (val) {
   console.log(val);
 });
 
-//map (key:value pairs)
+//Map (key:value pairs)
 let myMap = new Map([
   ["a1", "Hello"],
   ["b2", "Goodbye"],
@@ -45,3 +45,51 @@ let myMap = new Map([
 myMap.set("c3", "Foo");
 myMap.delete("a1");
 console.log(myMap.size);
+
+//WeakSet (objects - value)
+let carWeakSet = new WeakSet();
+
+let car1 = {
+  make: "Honda",
+  model: "Civic",
+};
+
+carWeakSet.add(car1);
+
+let car2 = {
+  make: "Toyota",
+  model: "Camry",
+};
+
+carWeakSet.add(car2);
+carWeakSet.delete(car1);
+
+console.log(carWeakSet);
+
+//WeakMap (objects - key: value)
+let carWeakMap = new WeakMap();
+
+let key1 = {
+  id: 1,
+};
+
+let testCar1 = {
+  make: "Honda",
+  model: "Civic",
+};
+
+let key2 = {
+  id: 2,
+};
+
+let testCar2 = {
+  make: "Toyota",
+  model: "Camry",
+};
+
+carWeakMap.set(key1, testCar1);
+carWeakMap.set(key2, testCar2);
+
+carWeakMap.delete(key1);
+
+console.log(carWeakMap);
